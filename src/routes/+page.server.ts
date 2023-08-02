3f13df60cfb41c03e896fb8d35ddd663accd3f89
bakkerjoeri <bakkerjoeri@gmail.com>
2023-08-02 23:34:12 +0200

    updated feeds

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,18 +3,21 @@ import type { PageServerLoad } from './$types';
 
 const feedUrls = [
 	'https://joy.recurse.com/feed.atom',
-	// 'https://decorrespondent.nl/feed/v1/publications',
 	'https://lostgarden.home.blog/feed',
 	'https://pudding.cool/rss.xml',
 	'https://qntm.org/rss.php',
-	'http://critical-distance.com/feed',
 	'http://www.nathalielawhead.com/candybox/feed',
 	'https://www.joshwcomeau.com/rss.xml',
 	'https://ltpf.ramiismail.com/blog/rss/',
 	'https://www.gridbugs.org/feed.xml',
 	'https://indiehellzone.com/feed/',
 	'https://100r.co/links/rss.xml',
-	'https://mixedinitiatives.net/atom.xml'
+	'https://mixedinitiatives.net/atom.xml',
+	'https://lwlies.com/feed/',
+	'https://www.anothergaze.com/feed/',
+	'https://decorrespondent.nl/feed/v1/profile/veramulder',
+	'https://decorrespondent.nl/feed/v1/profile/jessefrederik',
+	'https://decorrespondent.nl/feed/v1/profile/valentijndehingh'
 ];
 
 export const load = (async () => {
