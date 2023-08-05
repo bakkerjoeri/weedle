bc3eee9b464be1b35058c3258851742a1e41bf43
Joeri Bakker <bakkerjoeri@gmail.com>
2023-08-05 14:43:57 +0300

    added alistapart feed

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -19,7 +19,8 @@ const feedUrls = [
 	'https://decorrespondent.nl/feed/v1/profile/jessefrederik',
 	'https://decorrespondent.nl/feed/v1/profile/valentijndehingh',
 	'https://www.youtube.com/feeds/videos.xml?channel_id=UCeTfBygNb1TahcNpZyELO8g',
-	'https://www.youtube.com/feeds/videos.xml?channel_id=UCKv_QzXft4mD6TXmQBZtzIA'
+	'https://www.youtube.com/feeds/videos.xml?channel_id=UCKv_QzXft4mD6TXmQBZtzIA',
+	'https://alistapart.com/main/feed'
 ];
 
 export const load = (async () => {
