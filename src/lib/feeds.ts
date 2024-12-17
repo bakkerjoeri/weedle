d509fb5e0439b7734401e2b809732bd64f73df89
Joeri Bakker <bakkerjoeri@gmail.com>
2024-12-17 17:32:17 +0100

    fixed invalid dates

--- src/lib/feeds.ts
+++ src/lib/feeds.ts
@@ -1,5 +1,6 @@
 import Parser, { type Item, type Output } from 'rss-parser';
 import type { FeedInput } from '../feeds';
+import { isValid } from 'date-fns';
 
 export interface FeedItem extends Item {
 	feed: { slug: string } & Pick<
@@ -12,27 +13,32 @@ export interface FeedItem extends Item {
 const parser = new Parser();
 
 export async function getItemsFromFeed(feed: FeedInput): Promise<FeedItem[]> {
-	const feedOutput = await parser.parseURL(feed.url);
-	const items = feedOutput.items.map(
-		(item): FeedItem => ({
-			...item,
-			date: item.pubDate
-				? new Date(item.pubDate)
-				: item.isoDate
-				? new Date(item.isoDate)
-				: undefined,
-			feed: {
-				slug: feed.slug,
-				title: feed.title || feedOutput.title,
-				feedUrl: feedOutput.feedUrl,
-				link: feedOutput.link,
-				image: feedOutput.image,
-				description: feedOutput.description
-			}
-		})
-	);
+	try {
+		const feedOutput = await parser.parseURL(feed.url);
+		const items = feedOutput.items.map(
+			(item): FeedItem => ({
+				...item,
+				date:
+					item.pubDate && isValid(new Date(item.pubDate))
+						? new Date(item.pubDate)
+						: item.isoDate && isValid(new Date(item.isoDate))
+						? new Date(item.isoDate)
+						: undefined,
+				feed: {
+					slug: feed.slug,
+					title: feed.title || feedOutput.title,
+					feedUrl: feedOutput.feedUrl,
+					link: feedOutput.link,
+					image: feedOutput.image,
+					description: feedOutput.description
+				}
+			})
+		);
 
-	return items;
+		return items;
+	} catch {
+		return [];
+	}
 }
 
 export function sortFeedItemsByDate(feedItems: FeedItem[]): FeedItem[] {
